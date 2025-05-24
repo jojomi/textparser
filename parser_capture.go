fe98f81ccafe1db3811893a188398224372816f1
jojomi <dev.jojomi@yahoo.com>
2025-05-24 09:48:13 +0200

    Add (Must)Extract

--- parser_capture.go
+++ parser_capture.go
@@ -1,5 +1,7 @@
 package textparser
 
+import "fmt"
+
 // StartCapture starts collecting output, see Captured to retrieve it.
 func (x *Parser) StartCapture() *Parser {
 	x.captureStartPosition = x.CurrentIndex()
@@ -8,5 +10,27 @@ func (x *Parser) StartCapture() *Parser {
 
 // Captured retrieves output since StartCapture was last called.
 func (x *Parser) Captured() string {
-	return string(x.input[x.captureStartPosition:x.CurrentIndex()])
+	return x.MustExtract(x.captureStartPosition, x.CurrentIndex())
+}
+
+func (x *Parser) MustExtract(start, end int) string {
+	v, err := x.Extract(start, end)
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
+
+// Extract gets a substring of the input by indices.
+func (x *Parser) Extract(start, end int) (string, error) {
+	if start < 0 {
+		return "", fmt.Errorf("start index must be positive, got %d", start)
+	}
+	if end <= start {
+		return "", fmt.Errorf("end index must be greater than start index, got start %d, end %d", start, end)
+	}
+	if end > len(x.input) {
+		return "", fmt.Errorf("end index must be less than input length, got %d, input length %d", end, len(x.input))
+	}
+	return string(x.input[start:end]), nil
 }
--- parser_capture_test.go
+++ parser_capture_test.go
@@ -1,8 +1,9 @@
 package textparser
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestParser_Capture(t *testing.T) {
@@ -17,4 +18,34 @@ func TestParser_Capture(t *testing.T) {
 	p.MustSkip(1)
 
 	a.Equal("> b =", p.Captured())
+}
+
+func TestParser_Extract(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   string
+		start   int
+		end     int
+		want    string
+		wantErr bool
+	}{
+		{"basic", "a > b = 8", 0, 3, "a >", false},
+		{"tail", "a > b = 8", 6, 9, "= 8", false},
+		{"single rune", "a > b = 8", 2, 3, ">", false},
+		{"nothing", "a > b = 8", 3, 3, "", true},
+		{"bad start", "a > b = 8", -1, 3, "", true},
+		{"bad end", "a > b = 8", 0, 300, "", true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res, err := NewParser(tt.input).Extract(tt.start, tt.end)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.Nil(t, err)
+				assert.Equal(t, tt.want, res)
+			}
+		})
+	}
 }
